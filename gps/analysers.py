a3d7a98b695f697f09420180b47ab73dc02e5cfd
Live session user <you@example.com>
2018-01-30 16:43:42 +0000

    change xml_file, fix bug

--- gps/analysers.py
+++ gps/analysers.py
@@ -22,7 +22,7 @@ class Point_model(object):
         path_xml_file = Flight.objects.all().get(id=self.id).xml_file
         xml_file = (settings.MEDIA_ROOT + str(path_xml_file))
         tree = etree.parse(xml_file)
-        if str(Point.objects.all().filter(flight=Flight.objects.all().get(id=self.id))) == "<QuerySet []>":
+        if str(Point.objects.all().filter(flight=Flight.objects.all().get(id=self.id))) == "[]":
             for xml in tree.xpath("/trkseg/trkpt"):
                 Point(flight=Flight.objects.all().get(id=self.id), lat=float(xml.get("lat")), lon=float(xml.get("lon")), time=xml.get("hour"), c02=int(xml.get("co2")), nox=int(xml.get("nox")), nh3=int(xml.get("nh3"))).save()
 
@@ -44,5 +44,5 @@ class Maps(object):
         return points 
     
     def context(self):
-        data = {"data":{"points":self.points, "center":self.points()[0]}}
+        data = {"data":{"points":self.points(), "center":self.points()[0]}}
         return data
